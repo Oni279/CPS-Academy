cabea93a305809857575602f184ab27482793d9d
Farhana Mariyan Tarannum <mithioni279@gmail.com>
2025-11-11 02:57:26 +0600

    string

---
+++ Module1/Contest6-String/Codeforces_1703A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if (s[0] != 'Y' && s[0] != 'y')
+        {
+            cout << "NO" << endl;
+        }
+        else if( s[1] != 'E' && s[1] != 'e')
+        {
+            cout << "NO" << endl;
+        }
+        else if( s[2] != 'S' && s[2] != 's')
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+        
+    }
+    
+
+    return 0;
+}
