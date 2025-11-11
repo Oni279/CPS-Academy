a36ea280246251f23c1055782928bb26a4eb97d5
Farhana Mariyan Tarannum <mithioni279@gmail.com>
2025-11-12 02:19:16 +0600

    solved

---
+++ Module1/Contest6-String/Codeforces_1873A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string a = "abc";
+        string s;
+        cin >> s;
+        int cnt = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if( s[i] != a[i])
+            cnt++;
+        }
+        if (cnt <= 2)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+            
+    }
+    
+    return 0;
+}
